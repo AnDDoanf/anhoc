cb976ec6c2c94f403721bdcf75a8478f304f4d27
anddoanf-blueoc <an.doan@blueoc.tech>
2026-06-30 01:52:52 +0700

    chore: compile backend on GitHub Actions to bypass WASM memory limits

--- backend/passenger_entry.js
+++ backend/passenger_entry.js
@@ -1,5 +1,3 @@
 // passenger_entry.js
-// This script simply loads the TypeScript server.
-// For cPanel, make sure you configure the environment variable NODE_OPTIONS to '--import tsx'
-// so that Node.js can automatically compile and run the TypeScript files.
-import './server.ts';
+// This script loads the pre-compiled JavaScript server.
+import './dist/server.js';
